99bd8a5b631f27b7d50903492d1469b9c07947ea
MD YASIN ARAFAT <157967884+mdarafat17@users.noreply.github.com>
2024-05-23 22:57:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,20 @@
-let button1 = document.querySelector(".button01")
-let button2 = document.querySelector(".button02")
-let button3 = document.querySelector(".button03")
-let button4 = document.querySelector(".button04")
-let button5 = document.querySelector(".button05")
-let button6 = document.querySelector(".button06")
-let button7 = document.querySelector(".button07")
-let button8 = document.querySelector(".button08")
-let button9 = document.querySelector(".button09")
-let button0 = document.querySelector(".button0")
-let buttonPlus = document.querySelector(".buttonPlus")
-let buttonMainus = document.querySelector(".buttonMunus")
-let buttongun = document.querySelector(".buttonGun")
-let buttonVag = document.querySelector(".buttonVag")
-let buttonPercent = document.querySelector(".buttonPercent")
-let buttonAC = document.querySelector(".buttonAC")
-let buttonDEL = document.querySelector(".buttonDEL")
-let buttonEqual = document.querySelector(".buttonEqual")
-let count = document.querySelector(".Screen")
+let calBody = document.querySelector(".main")
+let btn = document.querySelectorAll("button")
+let screen = document.getElementById("Screen")
 
+for (let i = 0; i < btn.length; i++){
+  btn[i].addEventListener("click", () => {
+    if (btn[i].innerHTML == "="){
+    screen.innerHTML = eval(screen.innerHTML)
 
-console.log(button0);
-
-// button1.addEventListener('click', () => {
-//   let button1 = document.querySelector(".button01")
-//   count.innerHTML = 1
-// })
-
-// const btn1 = document.querySelector('.button01');
-// console.log(btn1);
-
-// .button2.addEventListener('click', () => {
-//   let button2 = document.querySelector(".button02")
-//   count.innerHTML = 2
-  
-// })
-
-// button1.addEventListener('click', () => button1('1'))
-
-
-// addEventListener('click', () => {
-//   let button1 = document.querySelector(".button01")
-//   count.innerHTML = +1
-// })
-
-
-// addEventListener('click', () => {
-//   let button1 = document.querySelector(".button01")
-//   count.innerHTML = +1
-// })
-
-// addEventListener('click', () => {
-//   let button1 = document.querySelector(".button01")
-//   count.innerHTML = +1
-// })
-
-
-// addEventListener('click', () => {
-//   let button1 = document.querySelector(".button01")
-//   count.innerHTML = +1
-// })
-
-
-// addEventListener('click', () => {
-//   let button1 = document.querySelector(".button01")
-//   count.innerHTML = +1
-// })
-
-
-// addEventListener('click', () => {
-//   let button1 = document.querySelector(".button01")
-//   count.innerHTML = +1
-// })
-
-
-
-
-
-
+  } else {
+      if (btn[i].innerHTML == "AC") {
+      screen.innerHTML = ''
+      }
+      
+      else {
+        screen.innerHTML += btn[i].innerHTML
+    }
+  }
+  })
+}
